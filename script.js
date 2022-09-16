d51bff8e53e6d2b474cba341ea938ffa2d5b6637
Aliyah Gutierrez <gutierrezaliyah@gmail.com>
2022-09-15 19:58:57 -0600

    deleting weird dot file

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
